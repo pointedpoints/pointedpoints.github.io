c5b8a362aba8064b7381e9542d129ef39d28c579
pointedpoints <1210322910@qq.com>
2019-07-23 23:09:28 +0800

    Site updated: 2019-07-23 23:09:26

--- js/next-boot.js
+++ js/next-boot.js
@@ -2,6 +2,8 @@
 
 $(document).ready(function() {
 
+  $(document).trigger('bootstrap:before');
+
   /**
    * Register JS handlers by condition option.
    * Need to add config option in Front-End at 'layout/_partials/head.swig' file.
@@ -11,7 +13,7 @@ $(document).ready(function() {
 
   NexT.utils.registerESCKeyEvent();
 
-  CONFIG.back2top.enable && NexT.utils.registerBackToTop();
+  CONFIG.back2top && NexT.utils.registerBackToTop();
 
   // Mobile top menu bar.
   $('.site-nav-toggle button').on('click', function() {
@@ -26,58 +28,26 @@ $(document).ready(function() {
     });
   });
 
+  /**
+   * Register JS handlers by condition option.
+   * Need to add config option in Front-End at 'layout/_partials/head.swig' file.
+   */
   CONFIG.fancybox && NexT.utils.wrapImageWithFancyBox();
-  CONFIG.copycode.enable && NexT.utils.registerCopyCode();
   CONFIG.tabs && NexT.utils.registerTabsTag();
 
   NexT.utils.embeddedVideoTransformer();
 
-  // Define Motion Sequence & Bootstrap Motion.
-  CONFIG.motion.enable && NexT.motion.integrator
+  // Define Motion Sequence.
+  NexT.motion.integrator
     .add(NexT.motion.middleWares.logo)
     .add(NexT.motion.middleWares.menu)
     .add(NexT.motion.middleWares.postList)
-    .add(NexT.motion.middleWares.sidebar)
-    .bootstrap();
+    .add(NexT.motion.middleWares.sidebar);
 
-  /**
-   * Init Sidebar & TOC inner dimensions on all pages and for all schemes.
-   * Need for Sidebar/TOC inner scrolling if content taller then viewport.
-   */
-  function updateSidebarHeight(height) {
-    height = height || 'auto';
-    $('.site-overview, .post-toc').css('max-height', height);
-  }
+  $(document).trigger('motion:before');
 
-  function initSidebarDimension() {
-    var updateSidebarHeightTimer;
+  // Bootstrap Motion.
+  CONFIG.motion.enable && NexT.motion.integrator.bootstrap();
 
-    $(window).on('resize', function() {
-      updateSidebarHeightTimer && clearTimeout(updateSidebarHeightTimer);
-
-      updateSidebarHeightTimer = setTimeout(function() {
-        var sidebarWrapperHeight = document.body.clientHeight - NexT.utils.getSidebarSchemePadding();
-
-        updateSidebarHeight(sidebarWrapperHeight);
-      }, 0);
-    });
-
-    // Initialize Sidebar & TOC Width.
-    var scrollbarWidth = NexT.utils.getScrollbarWidth();
-    if ($('.site-overview-wrap').height() > (document.body.clientHeight - NexT.utils.getSidebarSchemePadding())) {
-      $('.site-overview').css('width', 'calc(100% + ' + scrollbarWidth + 'px)');
-    }
-    if ($('.post-toc-wrap').height() > (document.body.clientHeight - NexT.utils.getSidebarSchemePadding())) {
-      $('.post-toc').css('width', 'calc(100% + ' + scrollbarWidth + 'px)');
-    }
-
-    // Initialize Sidebar & TOC Height.
-    updateSidebarHeight(document.body.clientHeight - NexT.utils.getSidebarSchemePadding());
-  }
-  initSidebarDimension();
-
-  function wrapTable() {
-    $('table').not('.gist table').wrap('<div class="table-container"></div>');
-  }
-  wrapTable();
+  $(document).trigger('bootstrap:after');
 });
