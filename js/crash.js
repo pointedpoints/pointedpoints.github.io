7a2129f8ec4c8321658634d8dbd379e33bdc3d8b
pointedpoints <1210322910@qq.com>
2019-07-28 17:18:15 +0800

    Site updated: 2019-07-28 17:18:12

--- js/crash.js
+++ js/crash.js
@@ -1,17 +1,17 @@
 <!--崩溃欺骗-->
-var OriginTitile = document.title;
+ var OriginTitle = document.title;
  var titleTime;
  document.addEventListener('visibilitychange', function () {
- if (document.hidden) {
-     $('[rel="icon"]').attr('href', "/img/TEP.ico");
-     document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
-     clearTimeout(titleTime);
- }
- else {
-     $('[rel="icon"]').attr('href', "/favicon.ico");
-     document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitile;
-     titleTime = setTimeout(function () {
-         document.title = OriginTitile;
-     }, 2000);
- }
+     if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/TEP.ico");
+         document.title = 'Free Pron Videos & Sex Movies|Prono,XXX,Pron Tube|PornHub';
+         clearTimeout(titleTime);
+     }
+     else {
+         $('[rel="icon"]').attr('href', "/favicon.ico");
+         document.title =  'rua!rua!!rua!!!'+ OriginTitle;
+         titleTime = setTimeout(function () {
+             document.title = OriginTitle;
+         }, 2000);
+     }
  });
